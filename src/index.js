4ced4208c4e829b0d1bfa6aeb85dbcc3878c1073
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-07-04 09:09:41 +0200

    done

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+// import { App } from 'components/example-1/App';
+import { App } from 'components/example-2/App';
+
+const theme = {
+  colors: {
+    black: '#212121',
+    white: '#fff',
+    accent: 'orangered',
+    error: 'red',
+  },
+  radii: {
+    xs: '2px',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+  },
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  // </React.StrictMode>
+);
